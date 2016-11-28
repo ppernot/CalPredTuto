899ff8dddb37977cf668db265f3a25109ac67bad
Pascal PERNOT <pascal.pernot@u-psud.fr>
2016-11-28 17:25:30 +0100

    update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "pascal.pernot@u-psud.fr"
+git config --global user.name "Pascal Pernot"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
